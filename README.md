cdc13a8c29acb07e1d3c135308bb11f9e9a8a255
lixin <1583810565@qq.com>
2020-01-09 11:54:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NetworkProgramming
+网络程序设计大作业，基于Socket套接字C语言编程
+server.conf定义服务器配置信息
+编译后运行客户端和服务器端即可，注意端口分配是否冲突
---
+++ client/main.c
@@ -0,0 +1,10 @@
+#include "lin.h"
+
+int main(int argc, char *argv[])
+{
+	// 初始化 gtk, 建立窗口
+	gtk_wnd_init(argc, argv);
+
+	return 0;
+}
+
